1a59cff6072bd1b99838d10055bcf9c9e42cf7e9
moajo <mimirosiasd@gmail.com>
2019-07-02 23:00:45 +0900

    add packrat test

---
+++ test/test_packrat.ts
@@ -0,0 +1,29 @@
+import { literal, or, sequence } from "../src/index"
+import ParserResolver, { ParseContext, ParserCache } from "../src/context"
+
+describe("expression", () => {
+  let c = new ParserResolver()
+  const kakko = literal("(")
+  const kokka = literal(")")
+  const one = literal("1")
+  const plus = literal("+")
+  const minus = literal("-")
+  const P = or(sequence(kakko, "A", kokka), one)
+  const A = or(sequence(P, plus, "A"), sequence(P, minus, "A"), P)
+
+  c.add("A", A)
+  c.add("S", A)
+
+  test("1", () => {
+    const S = c.get("S")
+    const pc = new ParseContext(new ParserCache(), c)
+    expect(S.parse(pc, "1")).not.toBe(null)
+  })
+  test("1", () => {
+    const S = c.get("S")
+    const pc = new ParseContext(new ParserCache(), c)
+    // note: this expression take too much time to parse if packrat-cache is disabled.
+    // ref: http://kmizu.hatenablog.com/entry/20090226/1235649181
+    expect(S.parse(pc, "(((((((((((((1)))))))))))))")).not.toBe(null)
+  })
+})
